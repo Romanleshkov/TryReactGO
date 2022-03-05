392cd55d7b066ed963ba5345e37aba4c7f3796b5
Romanleshkov <leshkovr@gmail.com>
2022-03-06 02:27:48 +0300

    backend mb done

---
+++ backend/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"backend/api"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	var handlers api.Handler
+	srv := new(api.Server)
+	if err := srv.Run(port, handlers.InitRoutes()); err != nil {
+		logrus.Fatalf("error occurred while running http server: %s", err.Error())
+	}
+}
