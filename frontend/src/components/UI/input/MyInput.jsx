ad3dd4dd01c7677fa22c2a42dd2355707d1c1628
Romanleshkov <leshkovr@gmail.com>
2022-03-20 23:08:16 +0300

    UI with css

---
+++ frontend/src/components/UI/input/MyInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from "./MyInput.module.css";
+
+const MyInput = React.forwardRef((props, ref)=> {
+    return (
+        <input {...props} className={classes.myInput} ref={ref}/>
+    );
+})
+
+export default MyInput;
