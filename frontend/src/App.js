9cd6773e0ab77b72b5673ac27b36c9dcf63e98c5
Romanleshkov <leshkovr@gmail.com>
2022-03-20 23:05:00 +0300

    Main fails

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useMemo, useState} from "react";
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = function () {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'ab', body: 'cd'},
+        {id: 2, title: 'cd', body: 'ef'},
+        {id: 3, title: 'ef', body: 'ab'}
+    ])
+
+    const [filter, setFilter] = useState({sort: '', query: ''})
+
+    const sortedPosts = useMemo(() => {
+        console.log("Sorted")
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts
+    }, [filter.sort, posts])
+
+
+    const sortedAndSearchedPosts = useMemo(()=>{
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+    }, [filter.query,sortedPosts])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+
+    return (
+        <div className="App">
+            <PostForm create={createPost}/>
+            <hr style={{margin: '15px'}}/>
+            <div>
+                <PostFilter
+                filter={filter}
+                setFilter={setFilter}/>
+            </div>
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Посты про JS"}/>
+        </div>
+    );
+};
+
 
 export default App;
